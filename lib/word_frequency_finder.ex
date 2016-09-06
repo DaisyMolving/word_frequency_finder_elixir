ff1923711d644c1deff15ff26bc04ddcac2f1bab
Daisy Molving <daisymolving@gmail.com>
2016-09-06 15:24:44 +0100

    sorts and prints words from most frequent to least, by selecting the most frequent and printing it before deleting it and recursing

--- lib/word_frequency_finder.ex
+++ lib/word_frequency_finder.ex
@@ -14,4 +14,19 @@ defmodule WordFrequencyFinder do
     |> tally_words(next_words)
   end
 
+  def sort_by_most_frequent(words_with_frequency) do
+    select_most_frequent(Map.to_list(words_with_frequency))
+  end
+
+  def select_most_frequent([]), do: 0
+  def select_most_frequent(words_with_frequency) do
+    most_frequent_word = words_with_frequency
+    |> Enum.max_by(fn{_, frequency} ->
+      frequency
+    end)
+    {word, highest_frequency} = most_frequent_word
+    IO.puts "#{word}: #{highest_frequency}"
+    select_most_frequent(List.delete(words_with_frequency, most_frequent_word))
+  end
+
 end
--- test/word_frequency_finder_test.exs
+++ test/word_frequency_finder_test.exs
@@ -11,4 +11,9 @@ defmodule WordFrequencyFinderTest do
     assert WordFrequencyFinder.tally_words(["hello", "hello", "there", "how", "many", "are", "there"]) == %{"hello"=> 2, "there" => 2, "how" => 1, "many" => 1, "are" => 1}
   end
 
+  test "prints words in order from highest to lowest frequency" do
+    assert capture_io(fn ->
+      WordFrequencyFinder.sort_by_most_frequent(%{"hello"=> 1, "there" => 1, "how" => 4, "many" => 2, "are" => 1})
+    end) == "how: 4\nmany: 2\nare: 1\nhello: 1\nthere: 1\n"
+  end
 end
