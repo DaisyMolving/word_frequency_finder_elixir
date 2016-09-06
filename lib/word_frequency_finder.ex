7bb856f2ae5fa2be639f3ed47ac9f60c59182449
Daisy Molving <daisymolving@gmail.com>
2016-09-06 15:21:01 +0100

    takes words and adds them with frequency to a map

--- lib/word_frequency_finder.ex
+++ lib/word_frequency_finder.ex
@@ -4,4 +4,14 @@ defmodule WordFrequencyFinder do
     String.split(text, ~r/(\W+)/, trim: true)
   end
 
+  def tally_words(word_list) do
+    tally_words(%{}, word_list)
+  end
+
+  def tally_words(words_with_frequency, []), do: words_with_frequency
+  def tally_words(words_with_frequency, [word | next_words]) do
+    Map.update(words_with_frequency, word, 1, &(&1 + 1))
+    |> tally_words(next_words)
+  end
+
 end
--- test/word_frequency_finder_test.exs
+++ test/word_frequency_finder_test.exs
@@ -1,9 +1,14 @@
 defmodule WordFrequencyFinderTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
   doctest WordFrequencyFinder
 
   test "splits every instance of a word in a list, not including punctuation" do
     assert WordFrequencyFinder.split_words("hello hello there, how many are there?") ==  ["hello", "hello", "there", "how", "many", "are", "there"] 
+  end
+
+  test "tallies the words in a map, counting their instances" do
+    assert WordFrequencyFinder.tally_words(["hello", "hello", "there", "how", "many", "are", "there"]) == %{"hello"=> 2, "there" => 2, "how" => 1, "many" => 1, "are" => 1}
   end
 
 end
